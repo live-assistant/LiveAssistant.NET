cd27ed8c31792386772204bfcc918f8ef3674e8c
John Cido <JohnCido@users.noreply.github.com>
2024-03-25 23:59:28 +0800

    Initial commit of Bilibili Danmaku Forward plugin;

---
+++ LiveAssistant.ExternalPlugin.BilibiliDanmakuForward/Main.cs
@@ -0,0 +1,41 @@
+﻿using BilibiliDM_PluginFramework;
+
+namespace LiveAssistant.ExternalPlugin.BilibiliDanmakuForward
+{
+    public class Main: DMPlugin
+    {
+        public Main()
+        {
+            PluginName = "Live Assistant Forward";
+            PluginDesc = "This plugin forwards Bilibili danmaku to Live Assistant desktop app.";
+            PluginAuth = "Live Assistant";
+            PluginVer = "v0.0.1";
+            PluginCont = "hello@live-assistant.app";
+
+            ReceivedDanmaku += OnReceivedDanmaku;
+            ReceivedRoomCount += OnReceivedRoomCount;
+        }
+
+        private void OnReceivedDanmaku(object sender, ReceivedDanmakuArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void OnReceivedRoomCount(object sender, ReceivedRoomCountArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Start()
+        {
+            base.Start();
+            Log("Live Assistant Forward started");
+        }
+
+        public override void Stop()
+        {
+            base.Stop();
+            Log("Live Assistant Forward stopped");
+        }
+    }
+}
---
+++ LiveAssistant.ExternalPlugin.BilibiliDanmakuForward/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LiveAssistant.ExternalPlugin.BilibiliDanmakuForward")]
+[assembly: AssemblyDescription("This plugin forwards Bilibili danmaku to Live Assistant desktop app.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LiveAssistant.ExternalPlugin.BilibiliDanmakuForward")]
+[assembly: AssemblyCopyright("Copyright (c) 2024 LiveAssistant.ExternalPlugin.BilibiliDanmakuForward Authors")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("59e3f154-d682-4ce8-b8ab-adde50932cf3")]
+
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyFileVersion("0.0.1")]
